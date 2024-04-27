233cfb83d6eea3d82527f63a625c65b46b2e8445
Mostakim Reza Omey <mostakim@Mostakims-MacBook-Air.local>
2024-04-28 03:00:59 +0600

    Python Basics

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
 print("goodbye")
+x = 1
+s = 1
+# Variables
+
+age = 14
+salary = 14.5
+# same line declaration
+boy_age, girl_age = 12, 14
+# multiple line declaration uses triple quotation
+des = """
+
+"""
