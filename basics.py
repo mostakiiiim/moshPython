578257bd673d48e2b2378d909ef0c96c9c5c66f6
Mostakim Reza Omey <mostakim@Mostakims-MacBook-Air.local>
2024-04-28 23:54:54 +0600

    Python Basics

---
+++ basics.py
@@ -0,0 +1,24 @@
+#Variables
+age = 14
+salary = 14.5
+#Dynamic Typing 
+#Type annotation
+#String
+#Escape Sequences
+#Formatted Strings
+#Useful String Methods
+#Numbers
+#Arithmetic Operators
+#Working with Numbers
+#Type Conversions
+#Logical Operators 
+#Ternary Operators
+#For Loop
+#For..Else
+#While Loops
+#Functions
+#Argument--Xargs
+#Argument--Xxargs
+#Scope
+#Debugging
+#Exercise 
